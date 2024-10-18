84c8e791357a9cbbf917af4ba773f37153eab4ed
asmmorshed101 <mm.morshed28707@gmail.com>
2024-10-19 02:40:10 +0900

    added navbar and create with section wise file. Need to watch carefully

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import DashboardWrapper from './dashboardWrapper'
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <DashboardWrapper>
         {children}
+        </DashboardWrapper>
       </body>
     </html>
   );
